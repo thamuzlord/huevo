ff183abf83910ee46aff57f6ec3accc9f6836ced
Leidy Zuluaga <leidyzuluaga@Leidys-MacBook-Pro.local>
2020-05-23 13:28:32 -0500

    web view

---
+++ app/src/main/java/com/mundo_huevo/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.mundo_huevo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webview.settings.javaScriptEnabled = true
+        webview?.loadUrl("https://developer.android.com/")
+
+
+
+    }
+}
+
